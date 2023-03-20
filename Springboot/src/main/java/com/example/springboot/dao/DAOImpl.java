ed4e6f75a5ce5ad46889b98d19990c94cfe8d431
DrKringe <Tuork@yandex.ru>
2023-03-20 18:25:22 +0300

    go

--- Springboot/src/main/java/com/example/springboot/dao/DAOImpl.java
+++ Springboot/src/main/java/com/example/springboot/dao/DAOImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 @Component
@@ -26,7 +26,6 @@ public class DAOImpl implements DAO {
     @Override
     public void addUser(Users users) {
         entityManager.persist(users);
-        entityManager.close();
     }
 
     @Override
--- Springboot/src/main/java/com/example/springboot/model/Users.java
+++ Springboot/src/main/java/com/example/springboot/model/Users.java
@@ -1,6 +1,6 @@
 package com.example.springboot.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
